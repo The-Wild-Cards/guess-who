f74f6f8c2070e613ff35f35e786f7b381ff0e295
Caleb Pendergraft <cackmed@gmail.com>
2019-10-21 14:54:17 -0700

    added findById, SaveUser, and GetUser functions to utilies.js

--- common/utilis.js
+++ common/utilis.js
@@ -1,1 +1,19 @@
-//comment
+export function findById(characterArray, id) {
+    for (let i = 0; i < characterArray.length; i++) {
+        const characterArray = characterArray[i];
+        if (characterArray.id === id) {
+            return characterArray;
+        }
+    }
+}
+export function saveUser(user) {
+    const jsonUser = JSON.stringify(user);
+    localStorage.setItem('user', jsonUser);
+}
+
+export function getUser() {
+    const getJsonUser = localStorage.getItem('user');
+    if (!getJsonUser) return null;
+    const user = JSON.parse(getJsonUser);
+    return user;
+}
