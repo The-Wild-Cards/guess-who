552f544e0dd5b0c509de8c5b91eb866c2d4fb3c0
Eve Stockstill <evestockstillpdx@gmail.com>
2019-10-23 17:45:43 -0700

    styled pages

--- home-page/home-page.js
+++ home-page/home-page.js
@@ -1,16 +1,16 @@
 import { saveUser, createUser } from '../common/utils.js';
 const userCreation = document.getElementById('user-creation');
 
-
-
 userCreation.addEventListener('submit', function(event) {
     event.preventDefault();
     const formData = new FormData(userCreation);
     const user = createUser(formData);
     saveUser(user);
-    window.location = '../game-page/index.html';
+    let chosenGameBoard = document.querySelector('input:checked').value;
+    if (chosenGameBoard === 'people'){
+        window.location = './Second-board/index.html';
+    } else window.location = '../game-page/index.html';
 });
-
 
 
 
