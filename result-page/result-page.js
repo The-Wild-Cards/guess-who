a3b2813b33617c1571ac256441a52c748a8897fb
Caleb Pendergraft <cackmed@gmail.com>
2019-10-24 10:39:50 -0700

    got things working

--- result-page/result-page.js
+++ result-page/result-page.js
@@ -11,8 +11,6 @@ const tableElement = document.querySelector('tbody');
 loadProfile(); 
 
 
-const computerCharacterImage = document.getElementById('correct-face');
-const characterId = getComputerCharacter();
 
 computerCharacterImage.src = ('../assets/faces/' + characterId + '.png');
 const javascriptReadableUserList = JSON.parse(localStorage.getItem(USER_LIST_KEY));
